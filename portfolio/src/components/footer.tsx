2610ad7227d565759f23d61a0329d87e95692aee
arbreshazeqiri <arbreshazeqiri0@gmail.com>
2025-07-26 15:03:06 +0200

    fix: mobile fix for credits

--- portfolio/src/components/footer.tsx
+++ portfolio/src/components/footer.tsx
@@ -3,10 +3,13 @@ import { Heart } from "lucide-react";
 const Footer = () => {
   return (
     <footer>
-      <div className="py-8 text-center">
+      <div className="flex lg:flex-row md:flex-row flex-col py-8 text-center justify-center gap-2">
         <p className="text-foreground/60 flex items-center justify-center gap-2">
-          © 2025 Arbresha Zeqiri. Made with{" "}
-          <Heart size={16} className="text-pink-gradient-start" />{" "} and a RedBull.
+          © 2025 Arbresha Zeqiri.{" "}
+        </p>
+        <p className="text-foreground/60 flex items-center justify-center gap-2">
+          Made with <Heart size={16} className="text-pink-gradient-start" /> and
+          a RedBull.
         </p>
       </div>
     </footer>
